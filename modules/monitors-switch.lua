c4f3c5b32ac2d187f492715551ef0535cc6d9330
Matthias Putz <matthias.putz@esrlabs.com>
2016-11-10 17:20:59 +0100

    added module for switching monitor with or without focused window

---
+++ modules/monitors-switch.lua
@@ -0,0 +1,38 @@
+local hotkey = require 'hs.hotkey'
+local window = require 'hs.window'
+local geometry = require 'hs.geometry'
+local mouse = require 'hs.mouse'
+local alert = require 'hs.alert'
+
+local position = import('utils/position')
+local monitors = import('utils/monitors')
+
+local function init_module()
+
+    hotkey.bind({ "ctrl", "alt", "cmd" }, "RETURN", function()
+        local win = window.focusedWindow()
+        if win == nil then
+            return
+        end
+        local frame = win:frame()
+        local destinationMonitor = monitors.configured_monitors[1]
+        local found = false
+        for id, monitor in pairs(monitors.configured_monitors) do
+            if  found then
+                destinationMonitor = monitor
+                break
+            end
+            --.. monitor.dimensions.x .. ", " .. monitor.dimensions.y)
+            if frame.x >= monitor.dimensions.x and frame.x < monitor.dimensions.x + monitor.dimensions.w and frame.y >= monitor.dimensions.y and frame.y < monitor.dimensions.y + monitor.dimensions.h then
+                found = true
+            end
+        end
+        if found then
+            win:setFrame(destinationMonitor.dimensions:relative_window_position(win))
+        end
+    end)
+end
+
+return {
+    init = init_module
+}
